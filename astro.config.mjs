91fca26ba8530badc345d1fe440f9fff9c556f99
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2022-11-02 11:08:45 -0400

    adding i18n spanish to navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),astroI18next()]
+  integrations: [tailwind(),astroI18next()],
+  site: 'https://weektodo.me'
 });
