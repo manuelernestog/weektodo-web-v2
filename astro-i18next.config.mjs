7f0804d9458a741c5f6f224696d1f4126a486e47
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2022-11-04 17:32:42 -0400

    changing navbar icons to webcomponets form iconfy

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
     defaultLanguage: "en",
-    supportedLanguages: ["en", "es"],
+    supportedLanguages: ["en", "es", "fr"],
     i18next: {
       debug: true,
       initImmediate: false,
