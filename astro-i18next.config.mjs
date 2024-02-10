c70b9e849fba9337f9e243d826eea3ab3b533366
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2024-02-10 09:38:51 -0500

    creating  hi pages

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
     defaultLanguage: "en",
-    supportedLanguages: ["en", "es", "fr", "de", "it", "ja", "ko", "pt", "ru", "tr", "uk", "zh"],
+    supportedLanguages: ["en", "es", "fr", "de", "it", "ja", "ko", "pt", "ru", "tr", "uk", "zh", "hi"],
+
+
     i18next: {
       debug: true,
       initImmediate: false,
+      supportedLngs: ["hi"],
       backend: {
         loadPath: "./src/locales/{{lng}}.json",
       },
