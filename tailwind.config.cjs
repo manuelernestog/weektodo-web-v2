59b6d3140e2dd1f76fb224624e37604fb7052480
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2024-02-11 00:05:41 -0500

    adding cello color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cello: {
+          50: "#f2f7fd",
+          100: "#e5edf9",
+          200: "#c5dbf2",
+          300: "#92bde7",
+          400: "#579ad9",
+          500: "#317ec6",
+          600: "#2263a7",
+          700: "#1d4f87",
+          800: "#1b4471",
+          900: "#183251",
+          950: "#13253e",
+        },
+      },
+    },
   },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
